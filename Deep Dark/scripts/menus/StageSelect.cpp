0fcbefaf9de3e369d9cada53995b870466fc48d3
ob7vo <termite678@gmail.com>
2025-10-27 13:36:29 -0400

    Update StageSelect.cpp

--- Deep Dark/scripts/menus/StageSelect.cpp
+++ Deep Dark/scripts/menus/StageSelect.cpp
@@ -4,15 +4,20 @@
 using namespace UI::StageSelect;
 
 StageSelect::StageSelect(Camera& cam) : Menu(cam){
-  
-    	std::string texPath = "sprites/ui/stage_select/stage_node.png";
-      for (int i = 0; i < STAGES; i++) {
-	        //stageBtn(i).set_new_params(STAGE_NODE_POS_ARR[i], STAGE_NODE_SIZE, texPath, cam);
-      }
+  	returnBtn().set_new_params(RETURN_BTN_POS, RETURN_BTN_SIZE,
+  	std::string texPath = "sprites/ui/stage_select/stage_node.png";
+	for (int i = 0; i < STAGES; i++) 
+        stageBtn(i).set_new_params({1.f,1.f}, STAGE_NODE_SIZE, texPath, cam);
+
+	std::string texPath2 = "sprites/ui/stage_select/return_btn.png";
+	returnBtn().set_new_params(RETURN_BTN_POS, RETURN_BTN_SIZE, texPath2, cam(;
+
+	reset_positions();
 }
 
 void StageSelect::reset_positions() {
-    for (int i = 0; i < STAGES; i++) {
-	       // stageBtn(i).sprite.setPosition(STAGE_NODE_POS_ARR[i], STAGE_NODE_SIZE, texPath, cam);
-      }
+    for (int i = 0; i < STAGES; i++) 
+	       stageBtn(i).sprite.setPosition(STAGE_NODE_POS_ARR[i]);
+
+	returnBtn().sprite.setPosition(RETURN_BTN_POS);
 }
