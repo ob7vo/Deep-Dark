1ab197c593174bffa9c4a206d7c4e06cd070ac3c
ob7vo <termite678@gmail.com>
2025-10-27 09:38:27 -0400

    Create StageSelect.h

---
+++ Deep Dark/scripts/menus/StageSelect.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Menu.h"
+
+const int STAGE_SELECT_BTNS = 4;
+const int STAGES = 3;
+
+struct StageSelect : public Menu<STAGE_SELECT_BTNS> {
+      sf::Text selectStageText = sf::Text(baseFont);
+
+      StageSelect(Camera& cam);
+      ~StageSelect() = default;
+
+      void draw() override;
+      void check_mouse_hover() override;
+      void register_click() override;
+
+      inline Button& stageBtn(int stage) {
+          return buttonManager.buttons[stage];
+      }
+      inline Button& returnBtn() {return buttonManager.buttons[STAGES];}
+}
