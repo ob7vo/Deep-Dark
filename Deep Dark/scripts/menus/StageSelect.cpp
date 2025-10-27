b4fd8e60a8406b3ddab2bbe1657fd4f421fcd1a5
ob7vo <termite678@gmail.com>
2025-10-27 10:17:36 -0400

    Update StageSelect.cpp

--- Deep Dark/scripts/menus/StageSelect.cpp
+++ Deep Dark/scripts/menus/StageSelect.cpp
@@ -5,4 +5,14 @@ using namespace UI::StageSelect;
 
 StageSelect::StageSelect(Camera& cam) : Menu(cam){
   
+    	std::string texPath = "sprites/ui/stage_select/stage_node.png";
+      for (int i = 0; i < STAGES; i++) {
+	        //stageBtn(i).set_new_params(STAGE_NODE_POS_ARR[i], STAGE_NODE_SIZE, texPath, cam);
+      }
+}
+
+void StageSelect::reset_positions() {
+    for (int i = 0; i < STAGES; i++) {
+	       // stageBtn(i).sprite.setPosition(STAGE_NODE_POS_ARR[i], STAGE_NODE_SIZE, texPath, cam);
+      }
 }
